4f7af3983d0f6f77824521431a3a9a0133e588e4
talhaarshad96 <arshad.talha18@gmail.com>
2021-03-22 13:47:14 +0500

    first commit

---
+++ tests/test.file.js
@@ -0,0 +1,12 @@
+module.exports = {
+    'Filling out form' : (browser) => {
+        browser.url('https://ultimateqa.com/filling-out-forms/')
+        .waitForElementVisible('body',1000)
+        .assert.title('Filling Out Forms - Ultimate QA')
+        .waitForElementVisible('#et_pb_contact_name_0')
+        .setValue('#et_pb_contact_name_0', 'Talha')
+        .click('.et_pb_contact_submit')
+        .waitForElementVisible('#et_pb_contact_form_0')
+        .expect.element('#et_pb_contact_name_0').text.to.equal('Form filled out')
+    }
+}
