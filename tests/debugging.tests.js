870d0364c981fb2ad4ea36573598fda8609a9472
talhaarshad96 <arshad.talha18@gmail.com>
2021-03-31 17:24:19 +0500

    blab

---
+++ tests/debugging.tests.js
@@ -0,0 +1,6 @@
+module.exports={
+    "should find results when searching for applitools":(browser)=>{
+        const ComplicatedPage = browser.page.ComplicatedPage();
+        const SearchPage = browser.page.SearchPage();
+    }
+}
